aa30624a10956f49e057671cda823b89796634eb
ludan12 <luis.padilla@yocontigo-it.com>
2023-01-30 10:24:41 -0600

    seleccionar asiento de mapa nodal

--- src/app/section/section.page.ts
+++ src/app/section/section.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { NavParams } from '@ionic/angular';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-section',
@@ -9,19 +10,62 @@ import { NavParams } from '@ionic/angular';
 })
 export class SectionPage implements OnInit {
   public params:any ;
+  public color:any;
+  public id:any;
+  public selected:any;
   constructor(private activatedRoute:ActivatedRoute,
     private navParams: NavParams) {
-      console.log(navParams.get('nombre'));
-    console.log(navParams.get('apellidos'));
-    console.log(navParams.get('locale'));
+      this.id = navParams.get('id');
+      if (navParams.get('section') == "VIP"){
+        this.color = "st0";
+      }else if (navParams.get('section') == "ORO"){
+        this.color = "st1";
+      }else if (navParams.get('section') == "PLATA1"){
+        this.color = "st2";
+      }else if (navParams.get('section') == "PLATA2"){
+        this.color = "st3";
+      }else if (navParams.get('section') == "PLATA3"){
+        this.color = "st4";
+      }else if (navParams.get('section') == "BRONCE"){
+        this.color = "st5";
+      }
     this.params = Object.assign({}, this.getParams());
   }
 
   ngOnInit() {
-    console.log(this.params);
   }
 
   getParams() {
     return this.activatedRoute.snapshot.params;
   }
+
+  selectSeat($seat:Event):void{
+    let seat:any =$seat.target;
+    let circle = document.getElementById(seat.dataset.seatId);
+    if (seat.dataset.isSele == 0){
+      circle?.setAttribute("class","st6");
+      seat.dataset.isSele = 1;
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+
+      Toast.fire({
+        icon: 'success',
+        title: 'Seleccionado',
+        text:'Asiento ' + seat.dataset.seatId
+      })
+    }else {
+      circle?.setAttribute("class",this.color);
+      seat.dataset.isSele =0;
+    }
+  }
+
 }
